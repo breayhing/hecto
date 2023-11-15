f29153792d40e4fd29d93cbd8f0317a660748954
breayhing <1519981563@qq.com>
2023-11-16 00:39:08 +0800

    feat:clear screen on exit

--- src/editor.rs
+++ src/editor.rs
@@ -28,7 +28,10 @@ impl Editor {
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
-        print!("{}",termion::clear::All,termion::cursor::Goto(1,1));
+        print!("{}{}",termion::clear::All,termion::cursor::Goto(1,1));
+        if self.should_quit{
+            println!("Goodbye.\r");
+        }
         io::stdout().flush()
     }
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
@@ -50,5 +53,6 @@ fn read_key() -> Result<Key, std::io::Error> {
 }
 
 fn die(e: std::io::Error) {
+    print!("{}",termion::clear::All);
     panic!("{}",e);
 }
