ac87eb9f0889e041eb2442791a3f27e514670c71
breayhing <1519981563@qq.com>
2023-11-16 22:10:08 +0800

    feat: print welcome message at the middle of the screen

--- src/editor.rs
+++ src/editor.rs
@@ -1,6 +1,8 @@
 use crate::Terminal;
 use termion::event::Key;
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
@@ -8,7 +10,6 @@ pub struct Editor {
 
 impl Editor {
     pub fn run(&mut self) {
-
         loop {
             if let Err(error) = self.refresh_screen() {
                 die(error);
@@ -49,16 +50,30 @@ impl Editor {
         }
         Ok(())
     }
+    fn draw_welcome_message(&self) {
+        let mut welcome_message = format!("Hecto editor -- version {}", VERSION);
+        let width = self.terminal.size().width as usize;
+        let len = welcome_message.len();
+        let padding = width.saturating_sub(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{}{}", spaces, welcome_message);
+        welcome_message.truncate(width);
+        println!("{}\r", welcome_message);
+    }
     fn draw_rows(&self) {
-        for _ in 0..self.terminal.size().height-1{
+        let height = self.terminal.size().height;
+        for row in 0..height - 1 {
             Terminal::clear_current_line();
-            println!("~\r");
+            if row == height / 3 {
+                self.draw_welcome_message();
+            } else {
+                println!("~\r");
+            }
         }
     }
 }
 
-
 fn die(e: std::io::Error) {
-    print!("{}", termion::clear::All);
-    panic!("{}", e);
+    Terminal::clear_screen();
+    panic!("{}",e);
 }
