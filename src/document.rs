76673cfbebbe81fc50c0e4caf4e843b2dd6e53c4
breayhing <1519981563@qq.com>
2023-11-17 15:56:18 +0800

    feat: complex deletion

--- src/document.rs
+++ src/document.rs
@@ -1,26 +1,26 @@
-use crate::Row;
 use crate::Position;
+use crate::Row;
 use std::fs;
 
 #[derive(Default)]
 pub struct Document {
     rows: Vec<Row>,
-    pub file_name:Option<String>,
+    pub file_name: Option<String>,
 }
 
 impl Document {
-    pub fn open(filename: &str ) -> Result<Self, std::io::Error> {
+    pub fn open(filename: &str) -> Result<Self, std::io::Error> {
         let contents = fs::read_to_string(filename)?;
         let mut rows = Vec::new();
         for value in contents.lines() {
             rows.push(Row::from(value));
         }
-        Ok(Self{
-            file_name:Some(filename.to_string()),
+        Ok(Self {
+            file_name: Some(filename.to_string()),
             rows,
         })
     }
-    pub fn len(&self) -> usize{
+    pub fn len(&self) -> usize {
         self.rows.len()
     }
     pub fn row(&self, index: usize) -> Option<&Row> {
@@ -29,21 +29,28 @@ impl Document {
     pub fn is_empty(&self) -> bool {
         self.rows.is_empty()
     }
-    pub fn insert(&mut self, at: &Position,c:char){
-        if at.y == self.len(){
+    pub fn insert(&mut self, at: &Position, c: char) {
+        if at.y == self.len() {
             let mut row = Row::default();
-            row.insert(0,c);
+            row.insert(0, c);
             self.rows.push(row);
-        }else if at.y < self.len(){
+        } else if at.y < self.len() {
             let row = self.rows.get_mut(at.y).unwrap();
-            row.insert(at.x,c);
+            row.insert(at.x, c);
         }
     }
-    pub fn delete(&mut self, at:&Position){
-        if at.y >= self.len(){
+    pub fn delete(&mut self, at: &Position) {
+        let len = self.len();
+        if at.y >= len {
             return;
         }
-        let row = self.rows.get_mut(at.y).unwrap();
-        row.delete(at.x);
+        if at.x == self.rows.get_mut(at.y).unwrap().len() && at.y < len - 1 {
+            let next_row = self.rows.remove(at.y + 1);
+            let row = self.rows.get_mut(at.y).unwrap();
+            row.append(&next_row);
+        } else {
+            let row = self.rows.get_mut(at.y).unwrap();
+            row.delete(at.x);
+        }
     }
 }
