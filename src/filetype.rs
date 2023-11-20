10fba9790a86d93da231cefa9a940379789ef0a7
breayhing <1519981563@qq.com>
2023-11-20 17:42:24 +0800

    feat: multi_line comment

--- src/filetype.rs
+++ src/filetype.rs
@@ -9,6 +9,7 @@ pub struct HighlightingOptions {
     strings: bool,
     characters: bool,
     comments: bool,
+    multiline_comments: bool,
     primary_keywords: Vec<String>,
     secondary_keywords: Vec<String>,
 }
@@ -38,6 +39,7 @@ impl FileType {
                     strings: true,
                     characters: true,
                     comments: true,
+                    multiline_comments: true,
                     primary_keywords: vec![
                         "as".to_string(),
                         "break".to_string(),
@@ -127,10 +129,13 @@ impl HighlightingOptions {
     pub fn comments(&self) -> bool {
         self.comments
     }
-    pub fn primary_keywords(&self) -> &Vec<String>{
+    pub fn primary_keywords(&self) -> &Vec<String> {
         &self.primary_keywords
     }
-    pub fn secondary_keywords(&self) -> &Vec<String>{
+    pub fn secondary_keywords(&self) -> &Vec<String> {
         &self.secondary_keywords
+    }
+    pub fn multiline_comments(&self) -> bool {
+        self.multiline_comments
     }
 }
--- src/highlighting.rs
+++ src/highlighting.rs
@@ -1,5 +1,5 @@
 use termion::color;
-#[derive(PartialEq,Clone,Copy)]
+#[derive(PartialEq, Clone, Copy)]
 pub enum Type {
     None,
     Number,
@@ -7,6 +7,7 @@ pub enum Type {
     String,
     Character,
     Comment,
+    MultilineComment,
     PrimaryKeywords,
     SecondaryKeywords,
 }
@@ -17,10 +18,10 @@ impl Type {
             Type::Number => color::Rgb(220, 163, 163),
             Type::Match => color::Rgb(38, 139, 210),
             Type::String => color::Rgb(211, 54, 130),
-            Type::Character => color::Rgb(108,113,196),
-            Type::Comment => color::Rgb(133,153,0),
-            Type::PrimaryKeywords => color::Rgb(181,137,152),
-            Type::SecondaryKeywords => color::Rgb(42, 161,152),
+            Type::Character => color::Rgb(108, 113, 196),
+            Type::Comment | Type::MultilineComment => color::Rgb(133, 153, 0),
+            Type::PrimaryKeywords => color::Rgb(181, 137, 0),
+            Type::SecondaryKeywords => color::Rgb(42, 161, 152),
             _ => color::Rgb(255, 255, 255),
         }
     }
